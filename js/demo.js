e787bbadb8f139d0a9628dd703d0a25755e0d6e3
Logan Garbarini <logangarbarini@gmail.com>
2016-04-05 02:52:02 -0400

    initial commit

---
+++ js/demo.js
@@ -0,0 +1,32 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#417DC1',
+    lineColor: '#417DC1',
+    curvedLines: true,
+    proximity: 300,
+    density: 50000,
+    minSpeedX: .5,
+    minSpeedY: .5
+  });
+  var intro = document.getElementById('intro');
+  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+}, false);
+
+
+/*
+// jQuery plugin example:
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+  });
+});
+*/
